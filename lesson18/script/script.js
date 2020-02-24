d172e0a441480df15b134e3a679430fca77e036e
Tosha Shigenina <shigenina@gmail.com>
2020-02-25 01:17:06 +0500

    таймер 24 час

--- lesson18/script/script.js
+++ lesson18/script/script.js
@@ -1,21 +1,25 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
 
+
   const countTimer = (deadline) => {
     const timerHours = document.getElementById('timer-hours'),
       timerMinutes = document.getElementById('timer-minutes'),
       timerSeconds = document.getElementById('timer-seconds');
 
+    const beautifyTime = (time) => {
+      if (time > 0 && time < 10) {
+        return `0${time}`;
+      } else if (time <= 0) {
+        return `00`;
+      }
+      return time;
+    };
+
+    let day = 24 * 60 * 60; //24 hours
+
     const getTimeRemaning = () => {
-        const beautifyTime = (time) => {
-            if (time > 0 && time < 10) {
-              return `0${time}`;
-            } else if (time <= 0) {
-              return `00`;
-            }
-            return time;
-          },
-          dateStop = new Date(deadline).getTime(),
+        const dateStop = new Date(deadline).getTime(),
           dateNow = new Date().getTime(),
           timeRemaining = (dateStop - dateNow) / 1000,
           seconds = beautifyTime(Math.floor(timeRemaining % 60)),
@@ -28,8 +32,24 @@ window.addEventListener('DOMContentLoaded', () => {
           seconds
         };
       },
+      dayTimer = () => {
+        day--;
+        if (day < 0) {
+          day = 24 * 60 * 60;
+        }
+
+        const seconds = beautifyTime(Math.floor(day % 60)),
+          minutes = beautifyTime(Math.floor((day / 60) % 60)),
+          hours = beautifyTime(Math.floor(day / 60 / 60));
+
+        return {
+          hours,
+          minutes,
+          seconds
+        };
+      },
       updateClock = () => {
-        const timer = getTimeRemaning();
+        const timer = dayTimer();
 
         timerHours.textContent = timer.hours;
         timerMinutes.textContent = timer.minutes;
