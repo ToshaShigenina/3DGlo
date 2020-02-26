9463f70ede664e16797ac7204d3828e3b767e834
Tosha Shigenina <shigenina@gmail.com>
2020-02-26 16:30:04 +0500

    Таймер исправлен, отсчитывает время до полуночи

--- lesson17/script/script.js
+++ lesson17/script/script.js
@@ -1,45 +1,28 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
 
-  const countTimer = (deadline) => {
+  const countTimer = () => {
     const timerHours = document.getElementById('timer-hours'),
       timerMinutes = document.getElementById('timer-minutes'),
       timerSeconds = document.getElementById('timer-seconds');
 
-    const beautifyTime = (time) => {
-      if (time > 0 && time < 10) {
-        return `0${time}`;
-      } else if (time <= 0) {
-        return `00`;
-      }
-      return time;
-    };
+    let now = new Date(),
+      tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1),
+      timeRemaning = Math.floor(tomorrow.getTime() - now.getTime()) / 1000;
 
-    let day = 24 * 60 * 60; //24 hours
-
-    const getTimeRemaning = () => {
-        const dateStop = new Date(deadline).getTime(),
-          dateNow = new Date().getTime(),
-          timeRemaining = (dateStop - dateNow) / 1000,
-          seconds = beautifyTime(Math.floor(timeRemaining % 60)),
-          minutes = beautifyTime(Math.floor((timeRemaining / 60) % 60)),
-          hours = beautifyTime(Math.floor(timeRemaining / 60 / 60));
-
-        return {
-          hours,
-          minutes,
-          seconds
+    const dayTimer = () => {
+        const beautifyTime = (time) => {
+          if (time > 0 && time < 10) {
+            return `0${time}`;
+          } else if (time <= 0) {
+            return `00`;
+          }
+          return time;
         };
-      },
-      dayTimer = () => {
-        day--;
-        if (day < 0) {
-          day = 24 * 60 * 60;
-        }
 
-        const seconds = beautifyTime(Math.floor(day % 60)),
-          minutes = beautifyTime(Math.floor((day / 60) % 60)),
-          hours = beautifyTime(Math.floor(day / 60 / 60));
+        const seconds = beautifyTime(Math.floor(timeRemaning % 60)),
+          minutes = beautifyTime(Math.floor((timeRemaning / 60) % 60)),
+          hours = beautifyTime(Math.floor(timeRemaning / 60 / 60));
 
         return {
           hours,
@@ -49,6 +32,7 @@ window.addEventListener('DOMContentLoaded', () => {
       },
       updateClock = () => {
         const timer = dayTimer();
+        timeRemaning--;
 
         timerHours.textContent = timer.hours;
         timerMinutes.textContent = timer.minutes;
@@ -58,7 +42,6 @@ window.addEventListener('DOMContentLoaded', () => {
     setInterval(updateClock, 1000);
   };
 
-  countTimer(`25 february 2020`);
-  //dayTimer();
+  countTimer();
 
 });
