99652ef0d95c794209c5ccd177ef90c3227921cb
Tosha Shigenina <shigenina@gmail.com>
2020-03-10 15:26:14 +0500

    Добавлены настройки вебпака

---
+++ lesson27/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.export = {
+  entry: {
+    main: './src/index.js'
+  },
+  outpt: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    publicPath: '/dist'
+  }
+};
