c99720ed4c2b7d69a804122a156322f545f0779c
Tosha Shigenina <shigenina@gmail.com>
2020-03-10 14:17:11 +0500

    Планый скролл

--- lesson27/script/script.js
+++ lesson27/script/script.js
@@ -15,37 +15,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   /* scroll */
 
-  const smoothScroll = () => {
-    const menu = document.querySelector('menu'),
-      serviceAnchor = document.querySelector('main a[href="#service-block"]');
-
-    const scrollToId = (anchor) => {
-      let elemId = anchor.getAttribute('href').slice(1);
-
-      document.getElementById(elemId).scrollIntoView({
-        block: "start",
-        behavior: "smooth"
-      });
-    };
-
-    menu.addEventListener('click', (event) => {
-      event.preventDefault();
-      let target = event.target.closest('li>a[href^="#"]');
-
-      if (!target) {
-        return;
-      }
-
-      scrollToId(target);
-    });
-
-    serviceAnchor.addEventListener('click', (event) => {
-      event.preventDefault();
-      scrollToId(serviceAnchor);
-    });
-
-  };
-
   smoothScroll();
 
   // tab
---
+++ lesson27/src/modules/smoothScroll.js
@@ -0,0 +1,30 @@
+const smoothScroll = () => {
+  const menu = document.querySelector('menu'),
+    serviceAnchor = document.querySelector('main a[href="#service-block"]');
+
+  const scrollToId = (anchor) => {
+    let elemId = anchor.getAttribute('href').slice(1);
+
+    document.getElementById(elemId).scrollIntoView({
+      block: "start",
+      behavior: "smooth"
+    });
+  };
+
+  menu.addEventListener('click', (event) => {
+    event.preventDefault();
+    let target = event.target.closest('li>a[href^="#"]');
+
+    if (!target) {
+      return;
+    }
+
+    scrollToId(target);
+  });
+
+  serviceAnchor.addEventListener('click', (event) => {
+    event.preventDefault();
+    scrollToId(serviceAnchor);
+  });
+
+};
