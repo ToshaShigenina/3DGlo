855776a94ead1c07e63bd6915a8ecf4376058321
Tosha Shigenina <shigenina@gmail.com>
2020-03-10 13:13:48 +0500

    Fetch добавлен

--- lesson26/script/script.js
+++ lesson26/script/script.js
@@ -458,22 +458,12 @@ window.addEventListener('DOMContentLoaded', () => {
     statusMessage.style.cssText = 'font-size: 2rem; color: #fff;';
 
     const postData = (body) => {
-        return new Promise((resolve, reject) => {
-          const request = new XMLHttpRequest();
-          request.addEventListener('readystatechange', () => {
-            if (request.readyState !== 4) {
-              return;
-            }
-            if (request.status === 200) {
-              resolve();
-            } else {
-              reject(request.status);
-            }
-          });
-
-          request.open('POST', './server.php');
-          request.setRequestHeader('Content-Type', 'application/json');
-          request.send(JSON.stringify(body));
+        return fetch('./server.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body)
         });
       },
       clearInput = (form) => {
@@ -495,14 +485,17 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
         postData(body)
-          .then(() => {
+          .then((response) => {
+            if (response.status !== 200) {
+              throw new Error(response.statusText);
+            }
             statusMessage.textContent = successMessage;
           })
           .catch((error) => {
             statusMessage.textContent = errorMessage;
           });
 
-        clearInput(form)
+        clearInput(form);
       };
 
     form1.addEventListener('submit', (event) => {
