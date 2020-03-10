322079e64291e11d7f9f2ade849b4b7386de95f9
Tosha Shigenina <shigenina@gmail.com>
2020-03-10 14:35:05 +0500

    Добавлен импорт

--- lesson27/src/index.js
+++ lesson27/src/index.js
@@ -1,37 +1,41 @@
 'use strict';
 
-//timer
+import countTimer from './modules/countTimer';
+import toggleMenu from './modules/toggleMenu';
+import togglePopUp from './modules/togglePopup';
+import smoothScroll from './modules/smoothScroll';
+import tabs from './modules/tabs';
+import slider from './modules/slider';
+import changeImg from './modules/changeImg';
+import calc from './modules/calc';
+import sendForm from './modules/sendForm';
+import checkInput from './modules/checkInput';
 
+
+
+//timer
 countTimer();
 
 //menu
-
 toggleMenu();
 
 //popup
-
 togglePopUp();
 
 /* scroll */
-
 smoothScroll();
 
 // tab
-
 tabs();
 
 // slider
-
 slider();
 
 // hover image
-
 changeImg();
 
 // calc
-
 calc(100);
 
 // send ajax form
-
 sendForm();
