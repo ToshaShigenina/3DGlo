17b51dc404786dfb3c06f235bcc34c005f608bca
Tosha Shigenina <shigenina@gmail.com>
2020-03-10 14:49:12 +0500

    Установлен webpack и собран main.js

--- lesson27/src/index.js
+++ lesson27/src/index.js
@@ -6,7 +6,7 @@ import togglePopUp from './modules/togglePopup';
 import smoothScroll from './modules/smoothScroll';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
-import changeImg from './modules/changeImg';
+import changeImg from './modules/changeImage';
 import calc from './modules/calc';
 import sendForm from './modules/sendForm';
 import checkInput from './modules/checkInput';
